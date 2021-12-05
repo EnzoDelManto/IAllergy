e27cddc3d27eefa6d95586c96851b2b744c093db
Enzo Del Manto <enzombdm@al.insper.edu.br>
2021-12-05 15:07:28 -0300

    correcao de erros

--- js/iallergy.js
+++ js/iallergy.js
@@ -1,25 +1,25 @@
 function EnviarF() {
     if (document.getElementById("GetValue1").value === "") {
-        alert("Por favor, digite seu nome")
+        alert("Por favor, digite seu nome");
     } else if (document.getElementById("GetValue2").value === "") {
-        alert("Por favor, digite um feedback")
+        alert("Por favor, digite um feedback");
     } else {
 
-        var criarHR = document.createElement("hr")
-        var criarN = document.createElement("div")
-        var pular = document.createElement("br")
-        var criarM = document.createElement("div")
-        criarN.innerHTML=document.getElementById("GetValue1").value + ":"
-        criarM.innerHTML=document.getElementById("GetValue2").value
-        document.body.appendChild(pular)
-        document.body.appendChild(pular)
-        document.body.appendChild(pular)
-        document.body.appendChild(criarN)
-        document.body.appendChild(pular)
-        document.body.appendChild(criarM)
-        document.body.appendChild(pular)
-        document.body.appendChild(pular)
-        document.body.appendChild(criarHR)
+        var criarHR = document.createElement("hr");
+        var criarN = document.createElement("div");
+        var pular = document.createElement("br");
+        var criarM = document.createElement("div");
+        criarN.innerHTML=document.getElementById("GetValue1").value + ":";
+        criarM.innerHTML=document.getElementById("GetValue2").value;
+        document.body.appendChild(pular);
+        document.body.appendChild(pular);
+        document.body.appendChild(pular);
+        document.body.appendChild(criarN);
+        document.body.appendChild(pular);
+        document.body.appendChild(criarM);
+        document.body.appendChild(pular);
+        document.body.appendChild(pular);
+        document.body.appendChild(criarHR);
             
     }
 }
